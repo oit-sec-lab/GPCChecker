c40d755c6b44dcf8d89f6ead8dd0c5d1fb839691
raqqona <e1n18047@st.oit.ac.jp>
2021-08-12 22:37:34 +0900

    fix path to URL

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
-function getURLs() {
-    $(".g").each(function() {
-        console.log($(this).prop('href'));
+function geturlss() {
+    let urls = {};
+
+    $(.yuRUbf a).each(function() {
+        urls[$(this).prop('a')] = $(this).prop('href');
     });
+    $(.yuRUbf link).each(function() {
+        urls[$(this).prop('link')] = $(this).prop('href');
+    });
+
+    var URL_JSON = JSON.stringify(urls);
+    console.log(URL_JSON);
 }
