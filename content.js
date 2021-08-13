c8b01bc39f04c097aae037dad9849c9c5dec449d
raqqona <e1n18047@st.oit.ac.jp>
2021-08-13 20:13:51 +0900

    fix how to add an id

--- content.js
+++ content.js
@@ -1,19 +1,22 @@
 function geturlss() {
-    var urls = [];
-    
-    $('.yuRUbf a').each(function(index) {
+    let urls = [];
+    var id = 1;
+
+    $('.yuRUbf a').each(function(id) {
         var url_json = {};
-        url_json['id'] = index + 1;
+        url_json['id'] = id;
         url_json['URL'] = $(this).prop('href');
         url_json['tag'] = 'a';
         urs.append(JSON.stringify(url_json));
+        id++;
     });
-    $('.yuRUbf link').each(function(index) {
+    $('.yuRUbf link').each(function(id) {
         var url_json = {};
-        url_json['id'] = index + 1;
+        url_json['id'] = id;
         url_json['URL'] = $(this).prop('href');
         url_json['tag'] = 'link';
         urs.append(JSON.stringify(url_json));
+        id++;
     });
     
     console.log(urls);
