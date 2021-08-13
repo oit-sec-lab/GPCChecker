400ad85e89618a2321e4a55e785299bb5bcf0058
raqqona <e1n18047@st.oit.ac.jp>
2021-08-13 21:26:35 +0900

    fix json format

--- content.js
+++ content.js
@@ -2,19 +2,19 @@ function geturlss() {
     let urls = [];
     var id = 1;
 
-    $('.yuRUbf a').each(function(id) {
+    $('.yuRUbf a').each(function() {
         var url_json = {};
-        url_json['id'] = id;
-        url_json['URL'] = $(this).prop('href');
-        url_json['tag'] = 'a';
+        url_json["id"] = id;
+        url_json["URL"] = $(this).prop('href');
+        url_json["tag"] = "a";
         urs.append(JSON.stringify(url_json));
         id++;
     });
-    $('.yuRUbf link').each(function(id) {
+    $('.yuRUbf link').each(function() {
         var url_json = {};
-        url_json['id'] = id;
-        url_json['URL'] = $(this).prop('href');
-        url_json['tag'] = 'link';
+        url_json["id"] = id;
+        url_json["URL"] = $(this).prop('href');
+        url_json["tag"] = "link";
         urs.append(JSON.stringify(url_json));
         id++;
     });
