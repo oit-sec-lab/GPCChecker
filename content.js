352fd96c7c15f3e84360bc5e3aca4e0cb6eee89f
raqqona <e1n18047@st.oit.ac.jp>
2021-08-15 23:03:14 +0900

    change json format and add throw API func

--- content.js
+++ content.js
@@ -1,24 +1,68 @@
-function geturlss() {
-    let urls = [];
+let URL = "***************"
+
+main();
+
+
+
+function getLinks() {
+    let link_json_array = [];
+    let bypass_id = {};
     var id = 1;
 
     $('.yuRUbf a').each(function() {
-        var url_json = {};
-        url_json["id"] = id;
-        url_json["URL"] = $(this).prop('href');
-        url_json["tag"] = "a";
-        urs.append(JSON.stringify(url_json));
+        var link_json = {};
+        link_json["id"] = id;
+        link_json["URL"] = $(this).prop('href');
+        link_json_array.append(JSON.stringify(link_json));
+
+        bypass[id] = $(this).prop('href');
+
         id++;
     });
     $('.yuRUbf link').each(function() {
-        var url_json = {};
-        url_json["id"] = id;
-        url_json["URL"] = $(this).prop('href');
-        url_json["tag"] = "link";
-        urs.append(JSON.stringify(url_json));
+        var link_json = {};
+        link_json["id"] = id;
+        link_json["URL"] = $(this).prop('href');
+        link_json_array.append(JSON.stringify(link_json));
+
+        bypass[id] = $(this).prop('href');
+
         id++;
     });
     
-    console.log(urls);
-    return urls;
+
+    return {
+        bypass_id,
+        link_json_array
+    };
+}
+
+
+function checkDNT(links) {
+    var response = fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: links
+    }
+
+    return response.json();
+}
+
+function main() {
+    var {bypass_link_from_id, link_json_array} = getLinks();
+    checkDNT(link_json_array)
+        .then((res) => {
+            if(!res.ok) {
+            throw new Error('Fetch: ${res.status} ${res.statusText}');
+            }
+            return res.json();
+        })
+        .then(json => {
+            console.log("kokomade");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
