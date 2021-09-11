f205bb085b1e907107763d6c7198844fc467d041
poro1225 <orient.bay0001@gmail.com>
2021-09-11 16:46:57 +0900

    fix isnert_icons Be car for changing fuction name and add web_accesible_resources to manifest.json

--- insert_icons.js
+++ insert_icons.js
@@ -1,25 +1,60 @@
-function insert_bool_icons(json_array) { //link_json_array == json_array
-
+//const IMG_TAG = '<img src="https://globalprivacycontrol.org/img/favicon/favicon-32x32.png">'
+const IMG_Path_true = chrome.extension.getURL('images/gpc_icon_true.jpg')
+const IMG_Path_false = chrome.extension.getURL('images/gpc_icon_false.jpg')
+const IMG_TAG_true = '<img src="${IMG_Path_true}">'
+const IMG_TAG_false = '<img src="${IMG_Path_false}">'
+function insert_gpc_icon(gpc_result) {
+    var id = 0;
     $('.yuRUbf a').each(function() {
-        var tags_url = $(this).prop('href');
-        if (tags_url.indexOf('webcache') == -1 && tags_url.indexOf('www.google.com/search?') == -1) {
-            const result_array = json_array.find(x => x.URL === tags_url);
-            const aEle = document.getElementById($('div > a'));
-            if(result_array.gpc){
-                console.log('true');
+        var URL = $(this).prop('href');
+        if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
+            var bool_1 = JSON.parse(gpc_result[id]);
+            if(bool_1.gpc){
+                $(this).children("br").after(IMG_TAG_true);
             }
             else{
-                console.log('false');
+                $(this).children("br").after(IMG_TAG_false);
             }
+            id++;
         }
     });
     $('.yuRUbf link').each(function() {
-
+        var URL = $(this).prop('href');
+        if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
+            var bool_1 = JSON.parse(gpc_result[id]);
+            if(bool_1.gpc){
+                $(this).children("br").after(IMG_TAG_true);
+            }
+            else{
+                $(this).children("br").after(IMG_TAG_false);
+            }
+            id++;
+        }
     });
     $('g-link link').each(function() {
-
+        var URL = $(this).prop('href');
+        if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
+            var bool_1 = JSON.parse(gpc_result[id]);
+            if(bool_1.gpc){
+                $(this).children("br").after(IMG_TAG_true);
+            }
+            else{
+                $(this).children("br").after(IMG_TAG_false);
+            }
+            id++;
+        }
     });
     $('g-link a').each(function() {
-
+        var URL = $(this).prop('href');
+        if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
+            var bool_1 = JSON.parse(gpc_result[id]);
+            if(bool_1.gpc){
+                $(this).children("br").after(IMG_TAG_true);
+            }
+            else{
+                $(this).children("br").after(IMG_TAG_false);
+            }
+            id++;
+        }
     });
 }
