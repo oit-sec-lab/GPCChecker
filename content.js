8bda24fe8b66f75a4f3fe2c4c7a28fffb02adaa1
poro1225 <orient.bay0001@gmail.com>
2021-09-22 12:14:51 +0900

    delete console log

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-let URL = "**********************"
+let URL = "**********"
 
 var link_json_array = getURLs();
 console.log(link_json_array);
@@ -6,9 +6,6 @@ console.log(link_json_array);
 checkGPC(URL, link_json_array)
     .then(res => res.json())
     .then(data => {
-        //"data" is json array from API response
-        //so, if you want to access each json, you can access like array
-        console.log(data);
         insert_gpc_icon(data);
     })
     .catch((error) => {
